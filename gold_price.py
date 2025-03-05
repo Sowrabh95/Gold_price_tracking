a0ac522fee2e933bee75cfc0d7827e691a4491e4
Sowrabh Kugunavar <135525276+Sowrabh95@users.noreply.github.com>
2025-03-06 01:44:50 +0530

    Update gold_price.py

--- gold_price.py
+++ gold_price.py
@@ -18,7 +18,7 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-url = "https://www.mmtcpamp.com/"
+url = " "# MMTC
 
 # Data storage for plotting
 timestamps = []
